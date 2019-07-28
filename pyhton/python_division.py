d8c073b9c99f295ab7658321a1ee4e78a82b8c28
Aishwarya <7aishwaryasharma@gmail.com>
2019-07-28 22:04:55 +0530

    python

---
+++ pyhton/python_division.py
@@ -0,0 +1,10 @@
+'''Read two integers and print two lines. The first line should contain integer division,  a//b . 
+The second line should contain float division,  a/b .
+'''
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
