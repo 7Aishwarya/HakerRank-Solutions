781560cd738b7c10f56183058d54b816365e1ade
Aishwarya <7aishwaryasharma@gmail.com>
2019-07-29 22:17:19 +0530

    python

--- Python/what's_your_name.py
+++ Python/what's_your_name.py
@@ -1,6 +1,7 @@
 '''You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
 
-Hello firstname lastname! You just delved into python.'''
+Hello firstname lastname! You just delved into python.
+'''
 
 
 def print_full_name(a, b):
