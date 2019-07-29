7b8f37da2749e0b2279f76f44c5859e875fe2c74
Aishwarya <7aishwaryasharma@gmail.com>
2019-07-29 22:10:14 +0530

    python

---
+++ Python/string_split_and_join.py
@@ -0,0 +1,7 @@
+def split_and_join(line):
+    # write your code here
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
+
+if __name__ == '__main__':
---
+++ Python/what's_your_name.py
@@ -0,0 +1,10 @@
+'''You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+Hello firstname lastname! You just delved into python.'''
+
+
+def print_full_name(a, b):
+    print("Hello",a,b,end = "")
+    print("! You just delved into python.")
+
+if __name__ == '__main__':
