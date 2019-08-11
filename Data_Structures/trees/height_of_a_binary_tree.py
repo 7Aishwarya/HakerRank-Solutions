21dae963e6558e442fcb713bc250e5742cae55ca
Aishwarya <7aishwaryasharma@gmail.com>
2019-08-12 00:44:41 +0530

    trees

---
+++ Data_Structures/trees/height_of_a_binary_tree.py
@@ -0,0 +1,73 @@
+'''Complete the getHeight or height function in the editor. It must return the height of a binary tree as an integer.'''
+
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+def height(root):
+    if root is None: 
+        return -1 ;  
+  
+    else : 
+        l = height(root.left) 
+        r = height(root.right) 
+        if (l > r): 
+            return l+1
+        else: 
+            return r+1
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
