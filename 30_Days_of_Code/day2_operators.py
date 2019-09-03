142324139f93cf6795a414151748c8107d0642d2
Aishwarya <7aishwaryasharma@gmail.com>
2019-09-03 23:13:09 +0530

    30 Days of Code

---
+++ 30_Days_of_Code/day2_operators.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost * tip_percent * 0.01
+    tax = meal_cost * tax_percent * 0.01
+    total = meal_cost + tip + tax
+    print(round(total))
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
