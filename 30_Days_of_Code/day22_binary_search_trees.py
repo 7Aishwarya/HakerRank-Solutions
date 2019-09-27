fd6e1935f5435f7c9e367bd012e999c1ec09a26f
Aishwarya <7aishwaryasharma@gmail.com>
2019-09-27 21:37:52 +0530

    30 Days of Code

---
+++ 30_Days_of_Code/day22_binary_search_trees.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def getHeight(self, root):
+        #Write your code here
+        if root is None:
+            return -1
+        else :
+            l = self.getHeight(root.left)
+            r = self.getHeight(root.right) 
+            if (l > r):
+                return l+1
+            else:
+                return r+1
+
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)       
