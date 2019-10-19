8a28bfa4172afe05f36f1d05d27357c980628b37
Aishwarya <7aishwaryasharma@gmail.com>
2019-10-19 11:19:08 +0530

    Java Loops

---
+++ Java/JavaLoops1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class JavaLoops1 {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        int i;
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(N+" x "+i+" = "+N*i);
+        }
+
+        scanner.close();
+    }
+}
